0ce26d14da587b0981a1e532b1cf3c0c19563f5d
crystaldust <juzhenatpku@gmail.com>
2018-06-06 10:53:07 +0800

    Get node ips from kubectl command. Get local ip from `ip addr` command

--- distribute-image.sh
+++ distribute-image.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
-ipsubnet=$1
-imagename=$2
+imagename=$1
+if [ "$imagename" == "" ]; then
+	echo "need an image name"
+	exit 1
+fi
+
 imagefilename=$(echo $imagename | sed 's/\//_/g')
 
-echo distributing $imagename
-echo saving to $imagefilename.tar
+# nodeIPs=$(kubectl get nodes -o wide | grep -v 'NAME.*STATUS' | awk '{print $1}')
+nodeIPs=$(kubectl get nodes -o jsonpath='{$.items[*].status.addresses[?(@.type=="ExternalIP")].address}')
+if [ "$nodeIPs" == "" ]; then
+	nodeIPs=$(kubectl get nodes -o jsonpath='{$.items[*].status.addresses[?(@.type=="InternalIP")].address}')
+fi
+if [ "$nodeIPs" == "" ]; then
+	nodeIPs=$(kubectl get nodes -o jsonpath='{$.items[*].status.addresses[?(@.type=="Hostname")].address}')
+fi
 
+if [ "$nodeIPs" == "" ]; then
+	echo "Failed to get nodes' IP"
+	exit 1
+fi
+
+
+localip=$(ip addr | grep 'inet.*wlp3s0' | awk '{print $2}' | awk '{split($0,a,"/")}; {print a[1]}')
+if [ "$localip" == "" ]; then
+	echo "Failed to get local ip"
+	exit 1
+fi
+
+echo saving to $imagefilename.tar
 docker save $imagename > /tmp/$imagefilename.tar
 
-scp /tmp/$imagefilename.tar root@192.168.${ipsubnet}0:/root/
-scp /tmp/$imagefilename.tar root@192.168.${ipsubnet}1:/root/
-scp /tmp/$imagefilename.tar root@192.168.${ipsubnet}2:/root/
-
-ssh root@192.168.${ipsubnet}0 "/usr/local/bin/docker load < /root/$imagefilename.tar"
-ssh root@192.168.${ipsubnet}1 "/usr/local/bin/docker load < /root/$imagefilename.tar"
-ssh root@192.168.${ipsubnet}2 "/usr/local/bin/docker load < /root/$imagefilename.tar"
+for nodeip in ${nodeIPs[@]}
+do
+	if [ "$nodeip" != "$localip" ]; then
+		echo distributing $imagename "to" $nodeip
+		scp /tmp/$imagefilename.tar root@$nodeip:/root/
+		ssh root@$nodeip "/usr/local/bin/docker load < /root/$imagefilename.tar"
+		ssh root@$nodeip "rm /root/$imagefilename.tar"
+	fi
+done
 
 rm /tmp/$imagefilename.tar
-ssh root@192.168.${ipsubnet}0 "rm /root/$imagefilename.tar"
-ssh root@192.168.${ipsubnet}1 "rm /root/$imagefilename.tar"
-ssh root@192.168.${ipsubnet}2 "rm /root/$imagefilename.tar"
